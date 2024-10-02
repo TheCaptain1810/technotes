8dd792a0d04e721f3c97369bc240600e08668ec9
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-10-02 18:32:20 +0530

    slices creation

--- src/app/api/appSlice.js
+++ src/app/api/appSlice.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery("https://localhost:3500"),
-    tagTypes: ["Note", "User"],
-    endpoints: builder => ({})
-})
+  baseQuery: fetchBaseQuery("https://localhost:3500"),
+  tagTypes: ["Note", "User"],
+  endpoints: (builder) => ({}),
+});
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
