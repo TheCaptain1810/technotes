e8cdb950b4947beb41e89d208c86181a20b409f2
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-10-02 17:29:52 +0530

    redux and rtk query basic structure

---
+++ src/app/api/appSlice.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const apiSlice = createApi({
+    baseQuery: fetchBaseQuery("https://localhost:3500"),
+    tagTypes: ["Note", "User"],
+    endpoints: builder => ({})
+})
